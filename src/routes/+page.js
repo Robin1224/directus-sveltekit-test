4ae36d77474237b642efff365c99faa6c4800c0b
Robin <robin.django@gmail.com>
2024-09-04 14:04:46 +0200

    Tutorial done

---
+++ src/routes/+page.js
@@ -0,0 +1,9 @@
+import getDirectusInstance from '$lib/directus';
+import { readItems } from '@directus/sdk';
+export async function load({ fetch }) {
+	const directus = getDirectusInstance(fetch);
+	return {
+		houses: await directus.request(readItems('f_houses')),
+
+	};
+}
